11bcc9234931d23149593a9c631a2456608c83dc
Galtamontan <alexandr.gavrik@mail.ru>
2017-08-19 16:54:20 +0400

    11
    111

---
+++ parts/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<title>Love to Life</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<link rel="shortcut icon" href="galery/favicon.png" type="image/png">
+	<style>
+	h1{font-size: 4rem;line-height: 1.6rem;margin:3rem;color:#633;}
+	h2{font-size: 6rem;margin:2.5rem;color:#933;text-shadow: 3px 3px 3px black;}
+	h5{font-size:1rem;color:#633;}
+	</style>
+</head>
+<body><!-- ------------------------------ B O D Y ---------------------------------------- -->
